d00a4d1bc7e958dff35e0c1bd99610d91b35e23f
vagrant <vagrant@localhost.localdomain>
2020-08-03 06:01:14 +0000

    update-new

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -13,13 +13,11 @@ class Admins::ProductsController < ApplicationController
   	   flash[:notice] = "Product was successfully created"
   	   redirect_to admins_products_show_path(@product.id)
   	else
-  	   @products = Products.all
   	   render :new
   	end
   end
 
   def show
-  	@product = Product.find(params[:id])
   end
 
   def edit
