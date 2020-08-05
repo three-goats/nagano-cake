8f2b8f1d0acf601dd87c01bb84f93198130b7941
vagrant <vagrant@localhost.localdomain>
2020-08-05 08:46:31 +0000

    add

--- app/controllers/members/products_controller.rb
+++ app/controllers/members/products_controller.rb
@@ -3,5 +3,9 @@ class Members::ProductsController < ApplicationController
   end
 
   def show
+  	@product = Product.find(params[:id])
+  	@types = ProductsType.all
+  	# 仮
+  	@basket_product = Product.new
   end
 end
