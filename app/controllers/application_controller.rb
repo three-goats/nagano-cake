468db5b4577f0840bcca86f12c9db4a5082d4aa7
vagrant <vagrant@localhost.localdomain>
2020-08-05 13:07:21 +0000

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
 	# (りのじ)_members/sign_upの際にemail.pasword以外にもカラムを追加するので
 	 before_action :configure_permitted_parameters, if: :devise_controller?
-	 
+
+	 # 下記のコードは注文_商品機能で使用
+	 protect_from_forgery with: :exception
+
+	 helper_method :current_order_product
+
+	 def current_order_product
+	 	if session[:order_products_id]
+	 	 @order_product = OrderProduct.find(session[:order_products_id])
+	 	else
+	 	 @order_product = OrderProduct.create
+	 	 session[:order_products_id] = @order_product.id
+	 	end
+	 end
+	 # 注文_商品機能ここまで
+
 	protected
 	def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name,
--- app/controllers/members/baskets_controller.rb
+++ app/controllers/members/baskets_controller.rb
@@ -10,14 +10,34 @@ class Members::BasketsController < ApplicationController
   	  redirect_to members_baskets_path, success: "商品をカートに追加しました。"
   	else
   	  @product = Product.find(@order_product.products_id)
+  	  render 'member/products/show'
+  	 end
   end
 
   def update
+  	@order_products = OrderProduct.find(params[:id])
+  	if @order_product.update(order_product_params)
+  	   redirect_to members_baskets_path, success: "個数を変更しました。"
+  	else
+  		render :index, danger: "個数の変更に失敗しました。"
+  	end
   end
 
   def destory
+  	@order_products = OrderProduct.find(params[:id])
+  	if @order_products.destory
+  	     redirect_to members_baskets_path, success: "商品の削除が完了しました。"
+  	else
+  		render :index, danger: "商品の削除ができませんでした。"
+  	end
   end
 
   def alldestory
+  	member = Member.find(current_member.id)
+  	if member.order_products.destroy.all
+  	   redirect_to members_baskets_path, success: "全商品の削除が完了しました。"
+  	else
+  		render :index, danger: "全商品の削除ができませんでした。"
+  	end
   end
 end
