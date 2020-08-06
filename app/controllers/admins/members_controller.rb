069975380ef27aedfde022bdb5669fa45146500b
vagrant <vagrant@localhost.localdomain>
2020-08-06 06:26:38 +0000

    [fix]members.controller.rb to pageing

--- app/controllers/admins/members_controller.rb
+++ app/controllers/admins/members_controller.rb
@@ -1,10 +1,10 @@
 class Admins::MembersController < ApplicationController
   def index
-  		@members = Member.page(params[:id]).reverse_order
+  		@members = Member.page(params[:page]).reverse_order
   end
 
   def show
-  	@member = Member.find(params[:id])
+  		@member = Member.find(params[:id])
   end
 
   def edit
