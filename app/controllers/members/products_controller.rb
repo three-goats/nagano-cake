a19ae666bbfa74bea3c4a32c5d0ef4c1ceea4c3b
vagrant <vagrant@localhost.localdomain>
2020-08-06 05:57:26 +0000

    update

--- app/controllers/members/products_controller.rb
+++ app/controllers/members/products_controller.rb
@@ -6,10 +6,14 @@ class Members::ProductsController < ApplicationController
   end
 
   def show
-  	@product = Product.find(params[:id])
+  	@product = Product.find(products_params[:id])
   	@order_product =OrderProduct.new
   	@types = ProductsType.where(validation: true)
   end
 
-  
+  private
+  def products_params
+  	params.require(:product).permit(:name, :explanation, :sales_status, :product_type_id, :unit_price)
+  end
+end
 end
