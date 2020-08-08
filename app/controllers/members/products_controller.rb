d0427822f6bba9755474c4cd4946996e1cb93dcd
vagrant <vagrant@localhost.localdomain>
2020-08-08 03:02:56 +0000

    update

--- app/controllers/members/products_controller.rb
+++ app/controllers/members/products_controller.rb
@@ -2,13 +2,13 @@ class Members::ProductsController < ApplicationController
   def index
   	@products = Product.where
   	@quantity = Product.count
-  	# @types = ProductsType.where
+  	@types = ProductsType.where
   end
 
   def show
   	@product = Product.find(params[:id])
-  	@order_product =OrderProduct.new
-  	# @types = ProductsType.where
+  	@order_product = OrderProduct.new
+  	@types = ProductsType.where
   end
 end
 
