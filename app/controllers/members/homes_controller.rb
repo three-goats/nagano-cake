81efb0b2f054cd281eec91c53d7fbf91e041e422
vagrant <vagrant@localhost.localdomain>
2020-08-10 05:00:29 +0000

    [update]

--- app/controllers/members/homes_controller.rb
+++ app/controllers/members/homes_controller.rb
@@ -1,10 +1,10 @@
 class Members::HomesController < ApplicationController
   def top
   	@types = ProductsType.all
-  	@product_1 = Product.find(5)
-  	@product_2 = Product.find(6)
-  	@product_3 = Product.find(7)
-  	@product_4 = Product.find(8)
+  	@product_1 = Product.find(1)
+  	@product_2 = Product.find(2)
+  	@product_3 = Product.find(3)
+  	@product_4 = Product.find(4)
   end
 
   def about
