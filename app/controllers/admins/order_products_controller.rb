80d2e3f239850ce9c3af6ea435184cdce746e7b3
vagrant <vagrant@localhost.localdomain>
2020-08-11 09:00:31 +0000

    [fix]admins-order

--- app/controllers/admins/order_products_controller.rb
+++ app/controllers/admins/order_products_controller.rb
@@ -5,7 +5,7 @@ class Admins::OrderProductsController < ApplicationController
 	  	
 	  	if @order_product.production_status == "製作中"
 	  		@order_product.order.update(order_status: 2)
-	  	elsif @order_product.order.order_products.all?{|op| op == "製作完了"}
+	  	elsif @order_product.order.order_products.all?{|op| op.production_status == "製作完了"}
 	  		@order_product.order.update(order_status: 3)
 	  	end
 	   	redirect_to admins_order_path(@order_product.order.id)
